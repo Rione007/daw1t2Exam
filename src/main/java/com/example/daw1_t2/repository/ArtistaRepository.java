b93e2c8de00a606ecc3878518ae4884de3f10b02
Rione <anderson020398.007@gmail.com>
2025-08-15 08:47:37 -0500

    no dormir pipipi

--- src/main/java/com/example/daw1_t2/repository/ArtistaRepository.java
+++ src/main/java/com/example/daw1_t2/repository/ArtistaRepository.java
@@ -8,19 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface ArtistaRepository extends CrudRepository<Artista, Integer> {
-    @Query("""
-SELECT new com.example.daw1_t2.dtos.TopArtistaDTO(
-    a.nombre,
-    a.paisOrigen,
-    a.generoMusical.name,
-    SUM(c.numeroReproducciones)
-)
-FROM Artista a
-LEFT JOIN a.canciones c
-GROUP BY a.id, a.nombre, a.paisOrigen, a.generoMusical
-ORDER BY SUM(c.numeroReproducciones) DESC
-""")
-    List<TopArtistaDTO> topArtistas();
 
 
 }
